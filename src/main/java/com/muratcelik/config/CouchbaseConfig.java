baaa38d0d5c60417f1df30c3b979e45f7b474e41
murat <muratcelik11@gmail.com>
2021-04-03 02:49:53 +0300

    Initial commit

---
+++ src/main/java/com/muratcelik/config/CouchbaseConfig.java
@@ -0,0 +1,35 @@
+package com.muratcelik.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.couchbase.config.AbstractCouchbaseConfiguration;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Murat Çelik
+ */
+@Configuration
+public class CouchbaseConfig extends AbstractCouchbaseConfiguration {
+
+
+    @Override
+    protected List<String> getBootstrapHosts() {
+        return Arrays.asList("127.0.0.1");
+    }
+
+    @Override
+    protected String getBucketName() {
+        return "db";
+    }
+
+    @Override
+    protected String getUsername(){
+        return "Administrator";
+    }
+
+    @Override
+    protected String getBucketPassword() {
+        return "pegamis";
+    }
+}
